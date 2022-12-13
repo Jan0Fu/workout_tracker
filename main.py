19e1d0f521fa3d4440983f34e7a0f825054aefd0
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-13 16:45:55 +0100

    Completed main program code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 APP_ID = "d5dea061"
 API_KEY = "7f7127f4e3df0448207a618a1c763da1"
@@ -12,6 +13,8 @@ header = {
     "x-app-key": API_KEY,
 }
 
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/23ff215de00c02d57c638f6c122e212d/myWorkouts/workouts"
 input_text = input("Tell me which exercises you did: ")
 
 params = {
@@ -21,8 +24,21 @@ params = {
     "height_cm": HEIGHT_CM,
     "age": AGE
 }
-exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 response = requests.post(url=exercise_endpoint, json=params, headers=header)
-data = response.json()
-print("data")
+result = response.json()
+
+today_date = datetime.now().strftime("%d%m%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
